22025ac40fb5e0b4f030fdb0d9af7a5cc886cbd5
vleypi <vleypi@gmail.com>
2022-12-30 02:47:59 +0500

    interface function props

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import Card, {CardVariant} from './components/Card'
 const App = () => {
     return (
       <div>
-          <Card variant={CardVariant.outlined} width='200px' height='200px'>
+          <Card onClick={() => console.log('click')} variant={CardVariant.outlined} width='200px' height='200px'>
             <button>Кнопка</button>
           </Card>
       </div>
--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -9,17 +9,21 @@ interface CardProps {
     width?: string;
     height: string;
     children: React.ReactNode,
-    variant: CardVariant
+    variant: CardVariant,
+    onClick: () => void; //number || string || void || true
 }
 
-const Card: FC<CardProps> = ({width,height,children,variant}) => {
+const Card: FC<CardProps> = ({width,height,children,variant,onClick}) => {
     return (
-      <div style={{
-        width,
-        height, 
-        border: variant === CardVariant.outlined ? 'gray 1px solid' : 'none',
-        background: variant === CardVariant.primary ? 'lightgray' : ''
-    }}>
+      <div 
+        style={{
+            width,
+            height, 
+            border: variant === CardVariant.outlined ? 'gray 1px solid' : 'none',
+            background: variant === CardVariant.primary ? 'lightgray' : ''
+        }}
+        onClick={onClick}
+    >
         {children}
       </div>
     )
