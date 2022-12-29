6f4183758f949b4958b063f5b30329edf2c3e598
vleypi <vleypi@gmail.com>
2022-12-30 02:37:51 +0500

    children props - ReactNode

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,9 @@ import Card from './components/Card'
 const App = () => {
     return (
       <div>
-          <Card width='200px' height='200px'/>
+          <Card width='200px' height='200px'>
+            <button>Кнопка</button>
+          </Card>
       </div>
     )
 }
--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -3,12 +3,13 @@ import React from 'react'
 interface CardProps {
     width?: string;
     height: string;
+    children?: React.ReactNode
 }
 
-const Card = ({width,height}: CardProps) => {
+const Card = ({width,height,children}: CardProps) => {
     return (
-      <div style={{width,height, background: 'gray'}}>
-
+      <div style={{width,height, border: 'gray 1px solid'}}>
+        {children}
       </div>
     )
 }
