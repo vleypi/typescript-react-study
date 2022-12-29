503a8847b715b1e57a70baa44d07c0803d1d0af6
vleypi <vleypi@gmail.com>
2022-12-30 02:45:35 +0500

    enum

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
 import React from 'react'
-import Card from './components/Card'
+import Card, {CardVariant} from './components/Card'
 
 const App = () => {
     return (
       <div>
-          <Card width='200px' height='200px'>
+          <Card variant={CardVariant.outlined} width='200px' height='200px'>
             <button>Кнопка</button>
           </Card>
       </div>
--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,14 +1,25 @@
 import React,{FC} from 'react'
 
+export enum CardVariant {
+    outlined = "outlined",
+    primary = "primary"
+}
+
 interface CardProps {
     width?: string;
     height: string;
-    children: React.ReactNode
+    children: React.ReactNode,
+    variant: CardVariant
 }
 
-const Card: FC<CardProps> = ({width,height,children}) => {
+const Card: FC<CardProps> = ({width,height,children,variant}) => {
     return (
-      <div style={{width,height, border: 'gray 1px solid'}}>
+      <div style={{
+        width,
+        height, 
+        border: variant === CardVariant.outlined ? 'gray 1px solid' : 'none',
+        background: variant === CardVariant.primary ? 'lightgray' : ''
+    }}>
         {children}
       </div>
     )
