6f1adbd3a9dfd2d89befa840263c47a34e47bca5
vleypi <vleypi@gmail.com>
2023-01-02 10:24:10 +0500

    axios request

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
-import React,{useState} from 'react'
+import axios from 'axios'
+import React,{useEffect, useState} from 'react'
 import Card, {CardVariant} from './components/Card'
 import UserList from './components/UserList'
 import { IUser } from './types/types'
@@ -6,6 +7,20 @@ import { IUser } from './types/types'
 const App = () => {
 
     const [users,setUsers] = useState<IUser[]>([])
+
+    useEffect(()=>{
+      fetchUsers()
+    },[])
+
+    const fetchUsers = async () =>{
+        try{
+            const res = await axios.get<IUser[]>('https://jsonplaceholder.typicode.com/users')
+            setUsers(res.data)
+        }
+        catch(err){
+            alert(err)
+        }
+    }
 
     return (
       <div>
